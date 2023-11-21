716d9f9275d09efe8d24d7f8d9e7f469905a49b0
Hoang Quang Huy <hoanghuy02062002@gmail.com>
2023-11-21 20:53:31 +0700

    [Feature] edit delete type in product

--- admin/type/index.php
+++ admin/type/index.php
@@ -1,24 +1,48 @@
 <?php
-    require '../../global.php';
-    require '../../dao/type.php';
+require '../../global.php';
+require '../../dao/type.php';
 
-    check_login();
+check_login();
 
-    extract($_REQUEST);
+extract($_REQUEST);
 
-    if(exist_param("add")){
-        try{
-            loai_insert($ten_loai);
-            unset($ten_loai, $ma_loai);
-            
-        }catch(Exception $exc){
-            $MESSAGE = 'Thêm mới thất bại!';
-        }
-        $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list"); 
-
-    }else{
-        $VIEW = 'type/new.php';
+if (exist_param("add")) {
+    try {
+        loai_insert($ten_loai);
+        unset($ten_loai, $ma_loai);
+    } catch (Exception $exc) {
+        $MESSAGE = 'Thêm mới thất bại!';
     }
+    $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list");
+} else if (exist_param('btn_update')) {
+    try {
+        loai_update($ma_loai, $ten_loai);
+    } catch (Exception $exc) {
+        $MESSAGE = 'Cập nhật thát bại!';
+    }
+    $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list");
+} else if (exist_param('btn_delete')) {
+    try {
+        loai_delete($ma_loai);
+        $types = loai_select_all();
+    } catch (Exception $exc) {
+        $MESSAGE = 'Xoá thất bại!';
+    }
+    $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list");
+} else if (exist_param('btn_delete_all')) {
+    try {
+        loai_delete_all();
+        $types = loai_select_all();
+    } catch (Exception $exc) {
+        $MESSAGE = 'Xoá thất bại!';
+    }
+    $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list");
+} else if (exist_param('btn_edit')) {
+    $type = loai_select_by_id($ma_loai);
+    extract($type);
+    $VIEW = 'type/edit.php';
+} else {
+    $VIEW = 'type/new.php';
+}
 
-    require '../layout.php';
-?>
+require '../layout.php';
