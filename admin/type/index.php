1d9c70e492800ad0bf12823af4c5ee3ee77093f8
Hoang Quang Huy <hoanghuy02062002@gmail.com>
2023-11-21 20:47:53 +0700

    [Feature] list type in product

--- admin/type/index.php
+++ admin/type/index.php
@@ -1,24 +1,25 @@
 <?php
-    require '../../global.php';
-    require '../../dao/type.php';
+require '../../global.php';
+require '../../dao/type.php';
 
-    check_login();
+check_login();
 
-    extract($_REQUEST);
+extract($_REQUEST);
 
-    if(exist_param("add")){
-        try{
-            loai_insert($ten_loai);
-            unset($ten_loai, $ma_loai);
-            
-        }catch(Exception $exc){
-            $MESSAGE = 'Thêm mới thất bại!';
-        }
-        $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list"); 
-
-    }else{
-        $VIEW = 'type/new.php';
+if (exist_param("add")) {
+    try {
+        loai_insert($ten_loai);
+        unset($ten_loai, $ma_loai);
+    } catch (Exception $exc) {
+        $MESSAGE = 'Thêm mới thất bại!';
     }
+    $VIEW = header("location: $ROOT_URL/admin/type/index.php?btn_list");
+} else if (exist_param('btn_list')) {
+    $types = loai_select_all();
+    $amounts = amount_type();
+    $VIEW = 'type/list.php';
+} else {
+    $VIEW = 'type/new.php';
+}
 
-    require '../layout.php';
-?>
+require '../layout.php';
