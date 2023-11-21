953f2bedf4ca7b5124323ded60afcc5fc4b277b6
Hoang Quang Huy <hoanghuy02062002@gmail.com>
2023-11-21 17:55:58 +0700

    [UI] show add edit delete type

---
+++ admin/type/new.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="../../content/css/css-admin/main.css">
+    <link rel="stylesheet" href="../../content/css/css-admin/form.css">
+    <link rel="stylesheet" href="../../content/css/css-admin/list.css">
+    <title>QUẢN TRỊ</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <!-- LAYOUT LEFT -->
+        <div class="layout-left hide">
+            <div class="sidebar-title">
+                <a href="<?= $SITE_URL ?>/main-page?/homepage"><img src="../../content/images/img-admin/logo.png" alt="Logo"></a>
+            </div>
+            <div class="sidebar-profile">
+                <div class="profile-pic">
+                    <img src="../../content/images/img-admin/img-users/cv_huy.jpg" alt="">
+                </div>
+                <div class="profile-info">
+                    <span>Xin chào</span>
+                    <h3>Huy kk</h3>
+                </div>
+            </div>
+            <!-- SIDEBAR MENU -->
+            <div class="sidebar-menu">
+                <div class="menu-section">
+                    <h3>Tổng quan</h3>
+                    <!-- PARENTS MENU -->
+                    <ul class="nav-side-menu">
+                        <li><a href="../main-page/index.php" class="link"><i class="fa-solid fa-house-user"></i>Trang chủ</a></li>
+                        <li>
+                            <a class="link"><label for="dropdown-product"><i class="fa-solid fa-bars"></i>Sản phẩm<i class="fa-solid fa-caret-down"></i></label></a>
+                            <input hidden type="checkbox" id="dropdown-product" class="dropdown-product">
+                            <!-- CHILD MENU -->
+                            <ul class="nav-child-menu">
+                                <li><a href="../type/list.php">Quản lý loại</a></li>
+                                <li><a href="../brand/list.php">Quản lý hãng</a></li>
+                                <li><a href="../goods/list.php">Quản lý sản phẩm</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="../customer/list.php" class="link"><i class="fa-solid fa-user"></i>Khách hàng</a></li>
+                        <li><a href="../comment/list.php" class="link"><i class="fa-solid fa-comment"></i>Bình luận</a></li>
+                        <li>
+                            <a href="../bill/list.php" class="link"><i class="fa-sharp fa-solid fa-briefcase"></i>Đơn hàng</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- LAYOUT RIGHT -->
+        <div class="layout-right">
+            <div class="wrap">
+                <div class="col">
+                    <div class="panel">
+                        <div class="title-panel">
+                            <h2>Thêm loại giày</h2>
+                        </div>
+                        <div class="content-noList"></div>
+                        <div class="content-panel">
+                            <form id="form" action="list.php" method="POST" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label class="control-label" for="id_type">Mã loại</label>
+                                    <input class="readonly form-control" type="text" name="ma_loai" id="id_type" readonly value="Auto number">
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label" for="name_type">Tên loại <strong>*</strong></label>
+                                    <input type="text" class="form-control" name="ten_loai" id="name_type" placeholder=" "> <br>
+                                    <label for="" class="label-field">Tên loại</label>
+                                </div>
+                                <div class="form-group-btn">
+                                    <button type="submit" name="add" class="btn_success">Lưu lại</button>
+                                    <button type="reset" class="btn_success_reset">Nhập lại</button>
+                                    <a href="./list.php" class="btn_list">Danh sách</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</body>
+
+</html>
+
+<script>
+    $(document).ready(function() {
+        $("#form").validate({
+            rules: {
+                'ten_loai': {
+                    required: true,
+                }
+            },
+
+            messages: {
+                'ten_loai': {
+                    required: "</br>Tên loại không được để trống!"
+                }
+            }
+        });
+    });
+</script>
