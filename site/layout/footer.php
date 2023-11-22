d85f11a3f15a34909464f7071069d4b71e6a6d50
vietnqdev-543 <vietnq543@gmail.com>
2023-11-22 10:14:16 +0700

    [UI] build footer for website

---
+++ site/layout/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div class="grid wide footer-content">
+        <div class="row no-gutters">
+            <div class="col l-3 m-3 c-12">
+                <div class="col-inner">
+                    <h2 class="footer-heading">Liên kết</h2>
+                    <ul class="footer-list">
+                        <li><a href="../main-page/index.php">Trang chủ</a></li>
+                        <li><a href="../main-page/about.php">Giới thiệu</a></li>
+                            <li><a href="../goods/list-allshop.php">Sản Phẩm</a></li>
+                        <li><a href="../main-page/contact.php">Liên hệ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col l-6 m-6 c-12 middle">
+                <div class="col-inner">
+                    <div class="footer-logo">
+                        <img src="../../content/images/img-site/logo.png" alt="Logo footer">
+                    </div>
+                    <p class="intro">Chúng tôi luôn không ngừng cố gắng, đem đến cho người tiêu dùng những
+                        sản phẩm mới, chất lượng với giá thành phù hợp!
+                    </p>
+                    <div class="social">
+                        <a href="https://www.facebook.com/"><img src="../../content/images/img-site/facebook.jpg" alt=""></a>
+                        <a href="https://www.instagram.com/"><img src="../../content/images/img-site/instagram.jpg" alt=""></a>
+                        <a href="https://www.tiktok.com/foryou"><img src="../../content/images/img-site/tiktok.jpg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col l-3 m-3 c-12">
+                <div class="col-inner">
+                    <h2 class="footer-heading">Thông tin liên hệ</h2>
+                    <div class="footer-contact">
+                        <p>Địa chỉ: 137 Nguyễn Thị Thập, Hòa Minh, Liên Chiểu, Đà Nẵng</p>
+                        <p>Số điện thoại: 0901749631</p>
+                        <p>Email: shopgiay@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-footer">
+                <p>© All rights reserved. Thiết kế website by KiXo Watch</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
