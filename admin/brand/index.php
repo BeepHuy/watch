efa18e47c6901b1fd9a46a16b9b02284af758bfa
phuc181003 <phuc07860@gmail.com>
2023-11-24 23:21:06 +0700

    [Feature] list delete brand in product

--- admin/brand/index.php
+++ admin/brand/index.php
@@ -1,18 +1,31 @@
-<?php require '../../global.php';
+<?php
+require '../../global.php';
 require '../../dao/brand.php';
 
 check_login();
 
 extract($_REQUEST);
 
-if (exist_param("add")) {
+if (exist_param('btn_delete')) {
     try {
-        hang_insert($ten_hang);
-        unset($ten_hang, $ma_hang);
+        hang_delete($ma_hang);
+        $brands = hang_select_all();
     } catch (Exception $exc) {
-        $MESSAGE = 'Thêm mới thất bại!';
+        $MESSAGE = 'Xoá thất bại!';
     }
     $VIEW = header("location: $ROOT_URL/admin/brand/index.php?btn_list");
+} else if (exist_param('btn_delete_all')) {
+    try {
+        hang_delete_all();
+        $brands = hang_select_all();
+    } catch (Exception $exc) {
+        $MESSAGE = 'Xoá thất bại!';
+    }
+    $VIEW = header("location: $ROOT_URL/admin/brand/index.php?btn_list");
+} else if (exist_param('btn_list')) {
+    $brands = hang_select_all();
+    $amounts = amount_brand();
+    $VIEW = 'brand/list.php';
 } else {
     $VIEW = 'brand/new.php';
 }
