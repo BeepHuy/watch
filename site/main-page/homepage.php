153e042d9a90dbd3029edf9567dc3f04247d3491
dinhtuan11 <tuanv2003@gmail.com>
2023-11-29 09:57:32 +0700

    hoan thanh

--- site/main-page/homepage.php
+++ site/main-page/homepage.php
@@ -212,39 +212,41 @@
             foreach ($products as $product) {
                 if ($product['giam_gia'] > 0) {
                     if ($i == 8) break; ?>
-                    <div class="col l-3 m-3 c-6" id="col-product">
-                        <a href="<?= $SITE_URL ?>/goods/detail.php?ma_sp=<?= $product['ma_sp'] ?>" class="product-item">
-                            <div class="content-product-item">
-                                <div class="product-item-discount">
-                                    <span>-<?= $product['giam_gia']; ?>%</span>
-                                </div>
-                                <div class="product-item-img">
-                                    <img src="<?= $CONTENT_URL ?>/images/img-admin/img-products/<?= $product['hinh']; ?>" alt="">
-                                </div>
-                                <div class="product-item-name-category">
-                                    <p><?= $product['ten_loai']; ?></p>
-                                </div>
-                                <div class="product-item-name">
-                                    <p><?= $product['ten_sp']; ?></p>
-                                </div>
-                                <div class="product-item-price">
-                                    <?php
+            <div class="col l-3 m-3 c-6" id="col-product">
+                <a href="<?= $SITE_URL ?>/goods/detail.php?ma_sp=<?= $product['ma_sp'] ?>" class="product-item">
+                    <div class="content-product-item">
+                        <div class="product-item-discount">
+                            <span>-<?= $product['giam_gia']; ?>%</span>
+                        </div>
+                        <div class="product-item-img">
+                            <img src="<?= $CONTENT_URL ?>/images/img-admin/img-products/<?= $product['hinh']; ?>"
+                                alt="">
+                        </div>
+                        <div class="product-item-name-category">
+                            <p><?= $product['ten_loai']; ?></p>
+                        </div>
+                        <div class="product-item-name">
+                            <p><?= $product['ten_sp']; ?></p>
+                        </div>
+                        <div class="product-item-price">
+                            <?php
                                     if ($product['giam_gia'] > 0) { ?>
-                                        <del><?= number_format($product['don_gia'], 0, ',', '.');  ?> đ</del>
-                                    <?php } ?>
-                                    <p><?= number_format($product['don_gia'] - ($product['don_gia'] * ($product['giam_gia'] / 100)), 0, ',', '.'); ?> đ</p>
-                                </div>
-                            </div>
-                        </a>
+                            <del><?= number_format($product['don_gia'], 0, ',', '.');  ?> đ</del>
+                            <?php } ?>
+                            <p><?= number_format($product['don_gia'] - ($product['don_gia'] * ($product['giam_gia'] / 100)), 0, ',', '.'); ?>
+                                đ</p>
+                        </div>
                     </div>
-                <?php $i++;
+                </a>
+            </div>
+            <?php $i++;
                 }
             }
             $href = "$SITE_URL/goods/listed.php?giam_gia";
             if (count($products) < 8) { ?>
-                <div class="show-more-product">
-                    <a href="<?= $href; ?>">Xem thêm</a>
-                </div> <?php } ?>
+            <div class="show-more-product">
+                <a href="<?= $href; ?>">Xem thêm</a>
+            </div> <?php } ?>
         </div>
     </div>
 
@@ -281,44 +283,45 @@
     </div>
 
     <div class="grid wide">
-        <!-- SẢN PHẨM XEM NHIỀU NHẤT -->
-        <div class="col l-12">
-            <h2 class="title-category">Sản phẩm Xem Nhiều Nhất</h2>
-        </div>
-        <div class="row no-gutters container-content">
-            <?php
-            foreach ($viewProducts as $product) { ?>
-                <div class="col l-3 m-3 c-6" id="col-product">
-                    <a href="<?= $SITE_URL ?>/goods/detail.php?ma_sp=<?= $product['ma_sp'] ?>" class="product-item">
-                        <div class="content-product-item">
-                            <!-- <?php
-                                    if ($product['giam_gia'] > 0) { ?>
+        <!-- SẢN PHẨM XEM NHIỀU NHẤT -->
+        <div class="col l-12">
+            <h2 class="title-category">Sản phẩm Xem Nhiều Nhất</h2>
+        </div>
+        <div class="row no-gutters container-content">
+            <?php
+foreach ($viewProducts as $product) { ?>
+            <div class="col l-3 m-3 c-6" id="col-product">
+                <a href="<?= $SITE_URL ?>/goods/detail.php?ma_sp=<?= $product['ma_sp'] ?>" class="product-item">
+                    <div class="content-product-item">
+                        <!-- <?php
+if ($product['giam_gia'] > 0) { ?>
                                         <div class="product-item-discount">
                                             <span>-<?= $product['giam_gia']; ?>%</span>
                                         </div>
                                 <?php } ?> -->
-                            <div class="product-item-img">
-                                <img src="<?= $CONTENT_URL ?>/images/img-admin/img-products/<?= $product['hinh']; ?>" alt="">
-                            </div>
-                            <div class="product-item-name-category">
-                                <p><?= $product['ten_loai']; ?></p>
-                            </div>
-                            <div class="product-item-name">
-                                <p><?= $product['ten_sp']; ?></p>
-                            </div>
-                            <div class="product-item-price">
-                                <!-- <?php
-                                        if ($product['giam_gia'] > 0) { ?>
-                                            <del><?= number_format($product['don_gia'], 0, ',', '.');  ?> đ</del>
+                        <div class="product-item-img">
+                            <img src="<?= $CONTENT_URL ?>/images/img-admin/img-products/<?= $product['hinh']; ?>"
+                                alt="">
+                        </div>
+                        <div class="product-item-name-category">
+                            <p><?= $product['ten_loai']; ?></p>
+                        </div>
+                        <div class="product-item-name">
+                            <p><?= $product['ten_sp']; ?></p>
+                        </div>
+                        <div class="product-item-price">
+                            <!-- <?php
+ if ($product['giam_gia'] > 0) { ?>
+                                            <del><?= number_format($product['don_gia'], 0, ',', '.'); ?> đ</del>
                                     <?php } ?> -->
-                                <p><?= number_format($product['don_gia'] - ($product['don_gia'] * ($product['giam_gia'] / 100)), 0, ',', '.'); ?> đ</p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            <?php } ?>
-        </div>
-    </div>
-</body>
+                            <p><?= number_format($product['don_gia'] - ($product['don_gia'] * ($product['giam_gia'] / 100)), 0, ',', '.'); ?>
+                                đ</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
 
 </html>
