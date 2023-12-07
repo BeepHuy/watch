0fa19998aa9a34fa1dd6af82402ebdf5cc88fe94
Datem1011 <datem1011@gmail.com>
2023-12-07 10:18:14 +0700

    feature/oder-payment

--- site/goods/show-cart.php
+++ site/goods/show-cart.php
@@ -55,99 +55,125 @@ if (!empty($_SESSION['cart'])) {
             <div class="row no-gutters">
                 <?php
                 if (isset($products) && count($products) > 0) { ?>
-                    <div class="col l-7 m-12 c-12">
-                        <form action="cart.php?action=submit" method="post">
-                            <div class="form-show-cart">
-                                <table class="tb-form-show-cart">
-                                    <thead>
-                                        <tr>
-                                            <th class="name-product" colspan="3">SẢN PHẨM</th>
-                                            <th class="price-product c-0">GIÁ</th>
-                                            <th class="amount-product">SỐ LƯỢNG</th>
-                                            <th class="total-product c-0">TẠM TÍNH</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
+                <div class="col l-7 m-12 c-12">
+                    <form action="cart.php?action=submit" method="post">
+                        <div class="form-show-cart">
+                            <table class="tb-form-show-cart">
+                                <thead>
+                                    <tr>
+                                        <th class="name-product" colspan="3">SẢN PHẨM</th>
+                                        <th class="price-product c-0">GIÁ</th>
+                                        <th class="amount-product">SỐ LƯỢNG</th>
+                                        <th class="total-product c-0">TẠM TÍNH</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
                                         $tong = 0;
 
                                         foreach ($products as $product) {
                                             $don_gia = $product['don_gia'] - ($product['don_gia'] * ($product['giam_gia'] / 100));
                                             $so_luong = $_SESSION['cart'][$product['ma_sp']]; ?>
-                                            <tr class="tr-product">
-                                                <td class="remove-product">
-                                                    <a href="cart.php?action=delete&id=<?= $product['ma_sp'] ?>" onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này không?')">
-                                                        <i class="fa-regular fa-circle-xmark"></i>
-                                                    </a>
-                                                </td>
-                                                <td class="img-product"><img src="<?= $CONTENT_URL ?>/images/img-admin/img-products/<?= $product['hinh'] ?>" alt=""></td>
-                                                <td class="name-product"><a href="<?= $SITE_URL ?>/goods/detail.php?ma_sp=<?= $product['ma_sp'] ?>"><?= $product['ten_sp'] ?></a>
-                                                    <p class="l-0 m-0">
-                                                        <span class="amount-product-mobile"><?= $so_luong; ?> x</span>
-                                                        <span class="price-product-mobile"><?= number_format($don_gia, 0, '.', ',') ?> <span style="text-decoration: underline;">đ</span></span>
-                                                    </p>
-                                                </td>
-                                                <td class="price-product c-0"> <?= number_format($don_gia, 0, '.', ','); ?> <span style="text-decoration: underline;">đ</span></td>
-                                                <td class="amount-product">
-                                                    <div class="quantity">
-                                                        <!-- <input type="button" value="-" class="btn-minus-quantity is-form"> -->
-                                                        <input type="number" name="quantity[<?= $product['ma_sp'] ?>]" class="ip-quantity" id="quantity" step="1" min="0" max="50" value="<?= $so_luong; ?>">
-                                                        <!-- <input type="button" value="+" class="btn-plus-quantity is-form"> -->
-                                                    </div>
-                                                </td>
-                                                <td class="total-product c-0"><?= number_format($don_gia * $so_luong, 0, '.', ',') ?> <span style="text-decoration: underline;">đ</span></td>
-                                            </tr>
-                                        <?php
+                                    <tr class="tr-product">
+                                        <td class="remove-product">
+                                            <a href="cart.php?action=delete&id=<?= $product['ma_sp'] ?>"
+                                                onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này không?')">
+                                                <i class="fa-regular fa-circle-xmark"></i>
+                                            </a>
+                                        </td>
+                                        <td class="img-product"><img
+                                                src="<?= $CONTENT_URL ?>/images/img-admin/img-products/<?= $product['hinh'] ?>"
+                                                alt=""></td>
+                                        <td class="name-product"><a
+                                                href="<?= $SITE_URL ?>/goods/detail.php?ma_sp=<?= $product['ma_sp'] ?>"><?= $product['ten_sp'] ?></a>
+                                            <p class="l-0 m-0">
+                                                <span class="amount-product-mobile"><?= $so_luong; ?> x</span>
+                                                <span
+                                                    class="price-product-mobile"><?= number_format($don_gia, 0, '.', ',') ?>
+                                                    <span style="text-decoration: underline;">đ</span></span>
+                                            </p>
+                                        </td>
+                                        <td class="price-product c-0"> <?= number_format($don_gia, 0, '.', ','); ?>
+                                            <span style="text-decoration: underline;">đ</span>
+                                        </td>
+                                        <td class="amount-product">
+                                            <div class="quantity">
+                                                <!-- <input type="button" value="-" class="btn-minus-quantity is-form"> -->
+                                                <input type="number" name="quantity[<?= $product['ma_sp'] ?>]"
+                                                    class="ip-quantity" id="quantity" step="1" min="0" max="50"
+                                                    value="<?= $so_luong; ?>">
+                                                <!-- <input type="button" value="+" class="btn-plus-quantity is-form"> -->
+                                            </div>
+                                        </td>
+                                        <td class="total-product c-0">
+                                            <?= number_format($don_gia * $so_luong, 0, '.', ',') ?> <span
+                                                style="text-decoration: underline;">đ</span></td>
+                                    </tr>
+                                    <?php
                                             $tong += $don_gia * $so_luong;
                                         } ?>
-                                        <tr>
-                                            <td class="td-continue-shopping" colspan="6">
-                                                <div class="continue-shopping">
-                                                    <a href="<?= $SITE_URL ?>/goods/listed.php?continue_shopping" class="button-continue-shopping">← TIẾP TỤC XEM SẢN PHẨM</a>
-                                                    <input type="submit" name="update_click" value="CẬP NHẬT GIỎ HÀNG" class="update-shopping">
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div class="col l-5 m-12 c-12">
-                        <div class="amount-show-cart">
-                            <table class="tb1">
-                                <thead>
                                     <tr>
-                                        <th class="name-product">CỘNG GIỎ HÀNG</th>
-                                    </tr>
-                                </thead>
-                            </table>
-                            <table class="tb2">
-                                <tbody>
-                                    <tr>
-                                        <th>Tạm tính</th>
-                                        <td><?= number_format($tong, 0, '.', ',') ?> <span style="text-decoration: underline;">đ</span></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Tổng</th>
-                                        <td><?= number_format($tong, 0, '.', ',') ?> <span style="text-decoration: underline;">đ</span></td>
+                                        <td class="td-continue-shopping" colspan="6">
+                                            <div class="continue-shopping">
+                                                <a href="<?= $SITE_URL ?>/goods/listed.php?continue_shopping"
+                                                    class="button-continue-shopping">← TIẾP TỤC XEM SẢN PHẨM</a>
+                                                <input type="submit" name="update_click" value="CẬP NHẬT GIỎ HÀNG"
+                                                    class="update-shopping">
+                                            </div>
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <div class="checkout">
-                                <a href="<?= $SITE_URL ?>/goods/cart.php?btn_form_cart" class="button-checkout">TIẾN HÀNH THANH TOÁN</a>
-                            </div>
                         </div>
-                    </div>
-                <?php } else { ?>
-                    <div class="col l-12 m-12 c-12">
-                        <div class="cart-empty">
-                            <p class="mes-cart-empty">Chưa có sản phẩm nào trong giỏ hàng.</p>
+                    </form>
+                </div>
 
-                            <p><a href="<?= $SITE_URL ?>/goods/listed.php?continue_shopping" class="back-shop">Quay trở lại cửa hàng</a></p>
+                <div class="col l-5 m-12 c-12">
+                    <div class="amount-show-cart">
+                        <table class="tb1">
+                            <thead>
+                                <tr>
+                                    <th class="name-product">CỘNG GIỎ HÀNG</th>
+                                </tr>
+                            </thead>
+                        </table>
+                        <table class="tb2">
+                            <tbody>
+                                <tr>
+                                    <th>Tạm tính</th>
+                                    <td><?= number_format($tong, 0, '.', ',') ?> <span
+                                            style="text-decoration: underline;">đ</span></td>
+                                </tr>
+                                <tr>
+                                    <th>Tổng</th>
+                                    <td><?= number_format($tong, 0, '.', ',') ?> <span
+                                            style="text-decoration: underline;">đ</span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="checkout">
+                            <a href="<?= $SITE_URL ?>/goods/cart.php?btn_form_cart" class="button-checkout">TIẾN HÀNH
+                                THANH TOÁN</a>
                         </div>
+                        <form class="" method="POST" target="_blank" enctype="application/x-www-form-urlencoded"
+                            action="../goods/xulymomoqrcode.php">
+                            <input type="submit" value="Thanh toán MOMO QRcode" name="momo">
+                        </form>
+                        <form class="" method="POST" target="_blank" enctype="application/x-www-form-urlencoded"
+                            action="../goods/xulymomoatm.php">
+                            <input type="submit" value="Thanh toán MOMO ATM" name="momo">
+                        </form>
                     </div>
+                </div>
+                <?php } else { ?>
+                <div class="col l-12 m-12 c-12">
+                    <div class="cart-empty">
+                        <p class="mes-cart-empty">Chưa có sản phẩm nào trong giỏ hàng.</p>
+
+                        <p><a href="<?= $SITE_URL ?>/goods/listed.php?continue_shopping" class="back-shop">Quay trở lại
+                                cửa hàng</a></p>
+                    </div>
+                </div>
                 <?php } ?>
             </div>
         </div>
